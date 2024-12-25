5849337feb9c644e6d1a417e768c153211664c42
BugHunter-X <askzenai@gmail.com>
2024-12-25 22:52:04 +0100

    Add RNVersionMismatchBug.js file

---
+++ RNVersionMismatchBug.js
@@ -0,0 +1,1 @@
+This error occurs when you are using a library that is not compatible with the version of React Native you are using. For example, if you are using React Native 0.64 and the library you are using only supports up to 0.63, you will get this error. 
